899652c9b74d5113a4d36983d8f17181b2a26f91
JohnAnon9771 <njoao97710@gmail.com>
2020-02-08 19:41:01 -0300

    added styles for home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
 
 import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
@@ -10,7 +11,13 @@ export default function Header() {
       <Link to="/">
         <img src={logo} alt="Rocketshoes" />
       </Link>
-      <Cart to="/cart"></Cart>
+      <Cart to="/cart">
+        <div>
+          <strong>Carrinho</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
     </Container>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
+import { MdShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_detalhe1.jpg?resize=280:280"
+          alt=""
+        />
+        <strong>Tênis muito show</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
