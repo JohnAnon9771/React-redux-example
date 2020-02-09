3f5b18442e45ae49d5cbb51faf2122784a7ff575
JohnAnon9771 <njoao97710@gmail.com>
2020-02-09 00:59:23 -0300

    added api and configuration

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { MdShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+
+import { formatPrice } from '../../utils/format';
 
 import { ProductList } from './styles';
 
 export default function Home() {
+  const [products, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get('products');
+      const data = response.data.map(product => ({
+        ...product,
+        price: formatPrice(product.price),
+      }));
+      setProduct(data);
+    }
+    getProducts();
+  }, []);
+
   return (
     <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_detalhe1.jpg?resize=280:280"
-          alt=""
-        />
-        <strong>Tênis muito show</strong>
-        <span>R$129,00</span>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt="" />
+          <strong>{product.title}</strong>
+          <span>{product.price}</span>
 
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+          <button type="button">
+            <div>
+              <MdShoppingCart size={16} color="#fff" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 }
