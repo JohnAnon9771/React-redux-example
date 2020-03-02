94ffd2174505a12224b6026fe6a59ca66a73baab
JohnAnon9771 <njoao9771@gmail.com>
2020-03-02 01:23:31 -0300

    added history for testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import './config/ReactotronConfig';
@@ -9,18 +9,20 @@ import store from './store/index';
 
 import { GlobalStyle } from './styles/global';
 
+import history from './services/history';
+
 import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
